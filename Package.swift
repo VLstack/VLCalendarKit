9174edd7bd12c99fd1be85d136aab8bbf24e630c
VLstack <github@vlstack.fr>
2024-03-25 09:36:24 +0100

    define dependency to VLDateKit

--- Package.swift
+++ Package.swift
@@ -14,11 +14,16 @@ let package = Package(
             name: "VLCalendarKit",
             targets: ["VLCalendarKit"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/VLstack/VLDateKit.git", from: "1.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "VLCalendarKit"),
+            name: "VLCalendarKit",
+            dependencies: ["VLDateKit"]),
         .testTarget(
             name: "VLCalendarKitTests",
             dependencies: ["VLCalendarKit"]),

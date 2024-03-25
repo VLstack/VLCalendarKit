ced6024865578c5097675b105c99d5e5e9f04cb1
VLstack <github@vlstack.fr>
2024-03-25 09:36:54 +0100

    implement "daysInMonthForCalendarDisplay"

--- Sources/VLCalendarKit/Date.swift
+++ Sources/VLCalendarKit/Date.swift
@@ -1,8 +1,32 @@
-//
-//  File.swift
-//  
-//
-//  Created by Laurent Vilday on 25/03/2024.
-//
-
 import Foundation
+import VLDateKit
+
+public extension Date
+{
+ var daysInMonthForCalendarDisplay: [ Date ]
+ {
+  var days: [ Date ] = []
+  // Current month days
+  let _startOfMonth: Date = self.startOfMonth
+  let _numberOfDaysInMonth: Int = self.numberOfDaysInMonth
+  for dayOffset in 0..<_numberOfDaysInMonth
+  {
+   let newDay: Date = Calendar.current.date(byAdding: .day, value: dayOffset, to: _startOfMonth) ?? .distantFuture
+   days.append(newDay)
+  }
+  
+  // Previous month days
+  let _startOfPreviousMonth: Date = self.startOfPreviousMonth
+  let _numberOfDaysInPreviousMonth: Int = _startOfPreviousMonth.numberOfDaysInMonth
+  
+  for dayOffset in 0..<_numberOfDaysInPreviousMonth
+  {
+   let newDay: Date = Calendar.current.date(byAdding: .day, value: dayOffset, to: _startOfPreviousMonth) ?? .distantPast
+   days.append(newDay)
+  }
+  
+  let _sundayBeforeStartOfMonth: Date = self.sundayBeforeStartOfMonth
+  let _endOfMonth: Date = self.endOfMonth
+  return days.filter { $0 >= _sundayBeforeStartOfMonth && $0 <= _endOfMonth }.sorted(by: <)
+ }
+}
